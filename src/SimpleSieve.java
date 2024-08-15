8665a3efab0e97bee838df573641088a764786cd
Hitesh Bhatnagar <125803878+hitesh-bhatnagar@users.noreply.github.com>
2024-08-15 10:48:53 +0530

    code for Simple Sieve algo.

---
+++ src/SimpleSieve.java
@@ -0,0 +1,30 @@
+public class SimpleSieve{
+    public static void simpleSieve(int limit){
+        boolean[] prime = new boolean[limit+1];
+
+        for (int i=2;i<=limit;i++){
+            prime[i] = true;
+        }
+
+        for (int p =2 ; p*p <= limit; p++){
+            if (prime[p] == true){
+
+                for(int i = p*p; i<=limit; i+=p ) {
+                    prime[i] = false;
+                }
+            }
+        }
+
+        for (int p = 2; p<=limit; p++ ) {
+            if (prime[p] == true) {
+                System.out.println(p+" ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        simpleSieve(50);
+    }
+}
